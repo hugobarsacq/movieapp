a22b480ccf7c64309435ee82fb6f3716b9926f52
hugobarsacq <hugo.barsacq.4O@gmail.com>
2022-04-15 12:21:26 +0200

    added error handling

--- lib/data/repositories/movie_repository_impl.dart
+++ lib/data/repositories/movie_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:movieapp/data/data_sources/movie_remote_data_source.dart';
 import 'package:movieapp/data/models/movie_model.dart';
@@ -15,38 +17,46 @@ class MovieRepositoryImpl extends MovieRepository {
     try {
       final movies = await remoteDataSource.getTrending();
       return Right(movies);
+    } on SocketException {
+      return Left(AppError(AppErrorType.network));
     } on Exception {
-      return Left(AppError('Something went wrong'));
+      return Left(AppError(AppErrorType.api));
     }
   }
 
   @override
-  Future<Either<AppError, List<MovieModel>>> getComingSoon() async {
+  Future<Either<AppError, List<MovieEntity>>> getComingSoon() async {
     try {
       final movies = await remoteDataSource.getComingSoon();
       return Right(movies);
+    } on SocketException {
+      return Left(AppError(AppErrorType.network));
     } on Exception {
-      return Left(AppError('Something went wrong'));
+      return Left(AppError(AppErrorType.api));
     }
   }
 
   @override
-  Future<Either<AppError, List<MovieModel>>> getPlayingNow() async {
+  Future<Either<AppError, List<MovieEntity>>> getPlayingNow() async {
     try {
       final movies = await remoteDataSource.getPlayingNow();
       return Right(movies);
+    } on SocketException {
+      return Left(AppError(AppErrorType.network));
     } on Exception {
-      return Left(AppError('Something went wrong'));
+      return Left(AppError(AppErrorType.api));
     }
   }
 
   @override
-  Future<Either<AppError, List<MovieModel>>> getPopular() async {
+  Future<Either<AppError, List<MovieEntity>>> getPopular() async {
     try {
       final movies = await remoteDataSource.getPopular();
       return Right(movies);
+    } on SocketException {
+      return Left(AppError(AppErrorType.network));
     } on Exception {
-      return Left(AppError('Something went wrong'));
+      return Left(AppError(AppErrorType.api));
     }
   }
 }
